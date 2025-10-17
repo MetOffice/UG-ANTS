f754ecfeadf778a2fa9218be00dbc39dc86cc1c2
Josh Rackham <jrackham-mo@users.noreply.github.com>
2025-10-17 14:44:35 +0100

    #4 Add UG-ANTS copyright to shell scripts

--- rose-stem/app/documentation/bin/build_docs.sh
+++ rose-stem/app/documentation/bin/build_docs.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -l
+# (C) Crown Copyright, Met Office. All rights reserved.
+#
+# This file is part of UG-ANTS and is released under the BSD 3-Clause license.
+# See LICENSE.txt in the root of the repository for full licensing details.
 
 # This is the canonical documentaion build.
 
