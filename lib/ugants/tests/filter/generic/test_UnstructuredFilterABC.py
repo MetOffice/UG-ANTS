72bde04a3803744fb20da3096fe9fcf3d341f4bd
Josh Rackham <jrackham-mo@users.noreply.github.com>
2025-10-17 14:41:58 +0100

    #4 Update copyright notices, ANTS -> UG-ANTS

--- lib/ugants/tests/filter/generic/test_UnstructuredFilterABC.py
+++ lib/ugants/tests/filter/generic/test_UnstructuredFilterABC.py
@@ -1,6 +1,6 @@
 # (C) Crown Copyright, Met Office. All rights reserved.
 #
-# This file is part of ANTS and is released under the BSD 3-Clause license.
+# This file is part of UG-ANTS and is released under the BSD 3-Clause license.
 # See LICENSE.txt in the root of the repository for full licensing details.
 from copy import deepcopy
 from unittest import mock
