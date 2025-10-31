b03dd3c734d817a2e66cb66c1b10d855958e33e2
Josh Rackham <jrackham-mo@users.noreply.github.com>
2025-10-31 18:26:38 +0000

    #8 Run pre-commit run -a

--- lib/ugants/regrid/applications.py
+++ lib/ugants/regrid/applications.py
@@ -4,6 +4,7 @@
 # This file is part of UG-ANTS and is released under the BSD 3-Clause license.
 # See LICENSE.txt in the root of the repository for full licensing details.
 """Implementation for the mesh to grid regrid application."""
+
 import os
 from pathlib import Path
 from typing import Literal
