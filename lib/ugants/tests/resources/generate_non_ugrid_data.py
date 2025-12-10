cfc6a94fbaaece7058ff9d90814ab1709e289cf4
Josh Rackham <144251043+jrackham-mo@users.noreply.github.com>
2025-12-10 13:07:06 +0000

    Add new source data and generation script

---
+++ lib/ugants/tests/resources/generate_non_ugrid_data.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""Generate non-ugrid source data for unit testing."""
+
+import iris
+import iris.coord_systems
+import ugants.tests.stock
+import ugants.utils.cube
+
+if __name__ == "__main__":
+    cube = ugants.tests.stock.regular_grid_global_cube(144, 192)
+    cube = ugants.utils.cube.prepare_for_save(cube)
+    coord_system = iris.coord_systems.GeogCS(6371229.0)
+    cube.coord(axis="x").coord_system = coord_system
+    cube.coord(axis="y").coord_system = coord_system
+    cube.attributes["source"] = (
+        f"Created using ugants.tests.stock.regular_grid_global_cube at UG-ANTS v{ugants.__version__}"  # noqa: E501
+    )
+    iris.save(cube, "non_ugrid_data.nc")
